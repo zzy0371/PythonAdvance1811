f57d07a863375a0a9ed046b308da1332ca1c13af
zzy <496575233@qq.com>
2019-02-13 16:34:48 +0800

    闭包

---
+++ day0213/demo07.py
@@ -0,0 +1,28 @@
+"""
+闭包：
+1，函数内部嵌套函数
+2，内部函数访问外部函数局部变量
+3，外部函数将内部函数返回
+
+因为外部函数返回的为内部函数的引用
+外部函数每执行一次都要返回内部函数的引用（该引用保存了外部函数的局部变量）
+
+"""
+
+def fun1():
+    a = 1
+    def fun2(b):
+        return a+b
+    return fun2
+
+# print(fun1, type(fun1))
+# print(fun1(), type(fun1()))
+# print(fun1()(2))
+
+r1 = fun1()
+print(r1(5))
+print(r1(5))
+print(r1(5))
+r2 = fun1()
+print(r1(6))
+
