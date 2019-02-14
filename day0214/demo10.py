024fc48aa471dae6162bf5bf66137ed59cebaea6
zzy <496575233@qq.com>
2019-02-14 15:15:00 +0800

    带参数装饰器

--- day0214/demo10.py
+++ day0214/demo10.py
@@ -1,17 +1,26 @@
 """
 最终装饰是语法格式
+带参数 带返回值的装饰器
 """
 
 def checkuser(fun):
-    def check():
+    def check(*args, **kwargs):
         username = input("请输入用户名")
         if username == "wzk":
-            fun()
+            return fun(*args, **kwargs)
         else:
             print("未授权")
     return check
 @checkuser
-def showlist():
-    print("展示订单列表")
+def showlist(n, all):
+    print("展示订单列表第"+str(n)+"页,共有"+str(all)+"页")
+@checkuser
+def showmoney():
+    print("显示余额")
+@checkuser
+def showinfo():
+    return "hello"
 
-showlist()
+showlist(10,100)
+showmoney()
+print(showinfo())
