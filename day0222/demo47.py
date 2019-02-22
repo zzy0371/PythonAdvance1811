07d6b1f1897e344c57259e271b2f8e484633bf3f
zzy <496575233@qq.com>
2019-02-22 16:31:58 +0800

    正则2

--- day0222/demo47.py
+++ day0222/demo47.py
@@ -67,5 +67,69 @@ re.S 单行模式，在单行模式下 .符号可以匹配\n
 
 ".*?"
 
-result = re.findall("\Wello", "hello aello5ello_ello .ello ^ello")
-print(result)
+# result = re.findall("\Wello", "hello aello5ello_ello .ello ^ello")
+# print(result)
+# result = re.findall("hi{1,2}", "hi china hello chiiina")
+# print(result)
+"""
+*可以匹配》=0
++可以匹配》=1
+？可以匹配0、1
+{m} 出现m次
+{m,} 出现》=m次
+{m,n}出现m-n次
+"""
+
+# result = re.findall("\\bhello\\b", "ahellohihhh_h_ hello")
+# print(result)
+"""
+^开头
+$结尾
+\b单词边界
+\B非单词边界
+"""
+
+# result = re.findall("hello.world","hello\nworld",re.S)
+# print(result)
+
+# result = re.match(r"(abc)aaabbc\1","abcaaabbcabc")
+# print(result.group(),result.group(1))
+
+# result = re.match(r"(?P<hname>hello) world (?P=hname)", "hello world hello china")
+# print(result.group(), result.group("hname"))
+
+# result = re.findall("e$","abcde\nabcd",re.M)
+# print(result)
+
+import requests
+def getinfo2():
+    response = requests.get("http://quote.stockstar.com/stock/ranklist_a_3_1_1.html")
+    content = response.text
+    result = re.findall(
+        '<td class="align_center "><a href="//stock.quote.stockstar.com/\d{6}.shtml">(\d{6})</a></td><td class="align_center"><a href="//stock.quote.stockstar.com/\d{6}.shtml">(.*?)</a>',
+        content)
+    return result
+
+def getinfo():
+    with open("content.txt", "r", encoding="utf-8") as f:
+        content = f.read()
+        # print(content)
+        result = re.findall(
+            '<td class="align_center "><a href="//stock.quote.stockstar.com/\d{6}.shtml">(\d{6})</a></td><td class="align_center"><a href="//stock.quote.stockstar.com/\d{6}.shtml">(.*?)</a>',
+            content)
+        return result
+
+def writefile():
+    # print(getinfo())
+    with open("result.txt", "w", encoding="utf-8") as f:
+        for r in getinfo2():
+            f.write(r[0]+"\t"+r[1])
+            f.write("\n")
+
+
+
+if __name__ == "__main__":
+    writefile()
+
+
+
